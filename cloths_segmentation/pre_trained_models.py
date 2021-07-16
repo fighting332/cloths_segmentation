cbca565c2c00ad7f8abd399d1cf8025b2bd0b24e
fighting332 <73146888+fighting332@users.noreply.github.com>
2021-07-16 09:13:31 +0900

    Update pre_trained_models.py

--- cloths_segmentation/pre_trained_models.py
+++ cloths_segmentation/pre_trained_models.py
@@ -9,7 +9,7 @@ model = namedtuple("model", ["url", "model"])
 
 models = {
     "Unet_2020-10-30": model(
-        url="https://github.com/fighting332/cloths_segmentation/releases/download/weights.zip",
+        url="https://github.com/ternaus/cloths_segmentation/releases/download/0.0.1/weights.zip",
         model=Unet(encoder_name="timm-efficientnet-b3", classes=1, encoder_weights=None),
     )
 }
